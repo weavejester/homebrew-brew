fbaf2d6577059c355b587e2f8f07e1dafe6c0ce3
James Reeves <jreeves@weavejester.com>
2026-02-12 15:02:28 +0000

    Update cljfmt to 0.16.0

--- Formula/cljfmt.rb
+++ Formula/cljfmt.rb
@@ -1,18 +1,18 @@
 class Cljfmt < Formula
   desc "A tool for formatting Clojure code"
   homepage "https://github.com/weavejester/cljfmt"
-  version "0.15.6"
+  version "0.16.0"
 
   if OS.linux?
-    url "https://github.com/weavejester/cljfmt/releases/download/0.15.6/cljfmt-0.15.6-linux-amd64-static.tar.gz"
-    sha256 "168a898d87211cd1c0c60dbb403b93a3846622daacc78066ec6fb99df101b4af"
+    url "https://github.com/weavejester/cljfmt/releases/download/0.16.0/cljfmt-0.16.0-linux-amd64-static.tar.gz"
+    sha256 "2e71751011505dda6d2735464f50bf64f11f47a01f353309666fd7a3d8fd15fd"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/weavejester/cljfmt/releases/download/0.15.6/cljfmt-0.15.6-darwin-aarch64.tar.gz"
-      sha256 "26154cb0a8fc561cbfbd33188f1bf2190759f42f5a071f8828d475230aa3f38c"
+      url "https://github.com/weavejester/cljfmt/releases/download/0.16.0/cljfmt-0.16.0-darwin-aarch64.tar.gz"
+      sha256 "f6558d864c2cf73e9e26bccd3e599aad79a27c614ca66c1eb2febe36dc635df5"
     else
-      url "https://github.com/weavejester/cljfmt/releases/download/0.15.6/cljfmt-0.15.6-darwin-amd64.tar.gz"
-      sha256 "ff7cdb37cc22854a618d35e28d381904b44bafdd951f9e0019230f7adeb67f00"
+      url "https://github.com/weavejester/cljfmt/releases/download/0.16.0/cljfmt-0.16.0-darwin-amd64.tar.gz"
+      sha256 "7407c1d3b74b698b0bd6da9bbbe9a7cf6c4747a0b2219a2040eef07b99efee51"
     end
   end
 
