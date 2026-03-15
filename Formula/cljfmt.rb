5b041ab5aaa7b812be1a6fefcfb81ae3ff41d94f
James Reeves <jreeves@weavejester.com>
2026-03-15 15:12:24 +0000

    Update cljfmt to 0.16.2

--- Formula/cljfmt.rb
+++ Formula/cljfmt.rb
@@ -1,16 +1,16 @@
 class Cljfmt < Formula
   desc "A tool for formatting Clojure code"
   homepage "https://github.com/weavejester/cljfmt"
-  version "0.16.1"
+  version "0.16.2"
 
   on_linux do
-    url "https://github.com/weavejester/cljfmt/releases/download/0.16.1/cljfmt-0.16.1-linux-amd64-static.tar.gz"
-    sha256 "1ebe27f48b453fa298ec4a1b249fea15fa9546c7e11ae4c366d252e2b8c84582"
+    url "https://github.com/weavejester/cljfmt/releases/download/0.16.2/cljfmt-0.16.2-linux-amd64-static.tar.gz"
+    sha256 "e653468a7d8e0f23b6344d0c4ea1446eb6072b46ff0f87675ccbe6f43b987cc6"
   end
 
   on_macos do
-    url "https://github.com/weavejester/cljfmt/releases/download/0.16.1/cljfmt-0.16.1-darwin-aarch64.tar.gz"
-    sha256 "37649399bfe22481be7558422e527065899bf5e1e4f7555db8ae63b8c6889749"
+    url "https://github.com/weavejester/cljfmt/releases/download/0.16.2/cljfmt-0.16.2-darwin-aarch64.tar.gz"
+    sha256 "e59d2d0f3f62829b3f54ad302b34d39a363d447c10af9a09e90c4c27abb0d89d"
   end
 
   def install
